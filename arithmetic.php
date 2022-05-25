3450ca5eb894eaa734881a7efd4c7b5b98265543
Festivitey <106256560+Festivitey@users.noreply.github.com>
2022-05-25 21:01:37 +0800

    Create arithmetic.php
    Update to 2 initial Files

---
+++ arithmetic.php
@@ -0,0 +1,16 @@
+<?php
+	// arithmetic.php
+	// perform arithmetic operations and assign the result to variables
+	$addition = 2 + 4;
+	$subtraction = 6 - 2;
+	$multiplication = 5 * 3;
+	$division = 15 / 3;
+	$modulus = 5 % 2;
+	
+	// Output the results
+	echo "Perform addition: 2 + 4 = ".$addition."<br>";
+	echo "Perform subtraction: 6 - 2 = ".$subtraction."<br>";
+	echo "Perform multiplication: 5 * 3 = ".$multiplication."<br>";
+	echo "Perform division: 15 / 3 = ".$division."<br>";
+	echo "Perform modulus : 5 % 2 =".$modulus
+		?>
